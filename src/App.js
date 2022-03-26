fdb294ac686d28370a67891b275f96fd8c7c0cfd
xo5378475 <xo5378475@gmail.com>
2022-03-26 09:04:42 +0800

    initial

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Button } from 'antd'
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+          App
+          <Button type='primary'> primary Button</Button>
+    </div>
+
+    )
+  }
+}
