c4c0bbf476ca74c7af96a0b85e40ea8cbce3850b
xo5378475 <xo5378475@gmail.com>
2022-04-01 13:44:35 +0800

    搭建Admin整体界面结构

--- src/pages/admin/admin.jsx
+++ src/pages/admin/admin.jsx
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
 import { Redirect } from 'react-router-dom'
 import memoryUtils from '../../utils/memoryUtils'
+import LeftNav from '../../components/left-nav'
+import Header from '../../components/header'
+import { Layout } from 'antd';
+
+const { Footer, Sider, Content } = Layout;
 
 // 後台管理的路由組件
 export default class Admin extends Component {
@@ -9,12 +14,21 @@ export default class Admin extends Component {
     const user = memoryUtils.user
     // 如果內存沒有儲存USER 表示當前沒有登錄
     if (!user || !user._id) {
-            // render 中使用跳轉
+      // render 中使用跳轉
       return <Redirect to='/login'></Redirect>
     }
 
     return (
-      <div>Hello {user.username}</div>
+      <Layout style={{height:"100%"}}>
+        <Sider>
+          <LeftNav></LeftNav>
+        </Sider>
+        <Layout>
+          <Header>Header</Header>
+          <Content style={{backgroundColor:'#fff'}}>Content</Content>
+          <Footer style={{textAlign:'center',color:'#ccc'}}>推薦使用谷歌瀏覽器，可以獲得更佳頁面操作體驗</Footer>
+        </Layout>
+      </Layout>
     )
   }
 }
