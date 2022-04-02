8dbe9a6f65c20af9e167278210fb47e2f4e00ed0
xo5378475 <xo5378475@gmail.com>
2022-04-03 01:28:30 +0800

    Header组件 静态界面

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -5,7 +5,20 @@ import './index.less'
 export default class Header extends Component {
   render() {
     return (
-      <div className='header'>Header</div>
+      <div className='header'>
+        <div className="header-top">
+          <span>歡迎, admin</span>
+          <a href="javascript:">退出</a>
+        </div>
+        <div className="header-bottom">
+          <div className="header-bottom-left">首頁</div>
+          <div className="header-bottom-right">
+            <span>2019-05-16 10:12:36</span>
+            <img src="" alt="" />
+            <span>晴</span>
+          </div>
+        </div>
+      </div>
     )
   }
 }
