10cd21363d80d1f979551d17a9c51eb8580f4303
xo5378475 <xo5378475@gmail.com>
2022-04-05 01:19:37 +0800

    退出登陆功能

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -5,6 +5,10 @@ import { formateDate } from '../../utils/dateUtils'
 import { withRouter } from 'react-router-dom'
 import memoryUtils from '../../utils/memoryUtils'
 import menuList from '../../config/menuConfig'
+import { Modal } from 'antd';
+import storageUtils from '../../utils/storageUtils'
+
+
 
 
  class Header extends Component {
@@ -15,7 +19,7 @@ import menuList from '../../config/menuConfig'
   }
 
   getTime = ()=>{
-    setInterval(()=>{
+    this.intervalId = setInterval(()=>{
       const currentTime = formateDate(Date.now())
       this.setState({currentTime})
     },1000)
@@ -42,12 +46,31 @@ import menuList from '../../config/menuConfig'
     this.getWeather()
   }
 
+  componentWillUnmount(){
+    clearInterval(this.intervalId)
+  }
+
   getWeather = async()=>{
     const weather = await reqWeather('高雄市')
     // var record = JSON.parse(weather.data)
     console.log(weather)
     this.setState({weather})
     // record = record.records.location[0].weatherElement[0].time[0].parameter.parameterName
+  }
+
+  logout = ()=>{
+    Modal.confirm({
+      content:'確定退出嗎？',
+      okText: '確定',
+      onOk:()=>{
+        console.log('ok',this);
+        // 刪除保存USer數據
+        storageUtils.removeUser()
+        memoryUtils.user = {}
+        this.props.history.replace('/login')
+      },
+      cancelText: '取消'
+    })
   }
 
   render() {
@@ -58,7 +81,7 @@ import menuList from '../../config/menuConfig'
       <div className='header'>
         <div className="header-top">
           <span>歡迎, {user}</span>
-          <a href="javascript:">退出</a>
+          <a href="javascript:" onClick={this.logout}>退出</a>
         </div>
         <div className="header-bottom">
           <div className="header-bottom-left">{this.title}</div>
