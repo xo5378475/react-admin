2a608ba36640faf83604f76cff9450c99bfd81fa
xo5378475 <xo5378475@gmail.com>
2022-04-04 23:22:53 +0800

    动态显示当前时间和天气

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 import ajax from "./ajax";
-
+import { message } from 'antd'
 
 // 登陸
 export const reqLogin = (username,password) => ajax('/login',{username,password},'POST')
@@ -8,6 +8,26 @@ export const reqLogin = (username,password) => ajax('/login',{username,password}
 export const reqAddUser = (user) => ajax('/manage/user/add',user,'POST')
 
 export const reqWeather = (city) => {
-   return ajax('/weather/get',{city},'GET')
+
+   return new Promise((resolve,reject)=>{
+      ajax('/weather/get',{city},'GET').then(
+         response=>{
+            var data = JSON.parse(response.data)
+            
+            if(data.success==='true'){
+               var weather = data.records.location[0].weatherElement[0].time[0].parameter.parameterName
+               resolve(weather)
+            } else{
+               message.error('獲取天氣失敗')
+               
+            }
+         }   
+      ).catch(err=>{
+         console.log(err)
+         message.error('獲取天氣失敗')
+      })
+
+   })
+    
 }
 
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,30 +1,51 @@
 import React, { Component } from 'react'
 import './index.less'
 import { reqWeather } from '../../api'
-
+import { formateDate } from '../../utils/dateUtils'
+import memoryUtils from '../../utils/memoryUtils'
 
 export default class Header extends Component {
 
-  getWeather = async(city)=>{
-    const weather = await reqWeather(city)
-    var record = JSON.parse(weather.data)
-    record = record.records.location[0].weatherElement[0].time[0].parameter.parameterName
+  state={
+    currentTime:formateDate(Date.now()),
+    weather:''
+  }
+
+  getTime = ()=>{
+    setInterval(()=>{
+      const currentTime = formateDate(Date.now())
+      this.setState({currentTime})
+    },1000)
+  }
+
+  componentWillMount(){
+    this.getTime()
+    this.getWeather()
+  }
+
+  getWeather = async()=>{
+    const weather = await reqWeather('高雄市')
+    // var record = JSON.parse(weather.data)
+    console.log(weather)
+    this.setState({weather})
+    // record = record.records.location[0].weatherElement[0].time[0].parameter.parameterName
   }
 
   render() {
-    this.getWeather('高雄市')
+    const {weather,currentTime} = this.state
+    const user = memoryUtils.user.username
     return (
       <div className='header'>
         <div className="header-top">
-          <span>歡迎, admin</span>
+          <span>歡迎, {user}</span>
           <a href="javascript:">退出</a>
         </div>
         <div className="header-bottom">
           <div className="header-bottom-left">首頁</div>
           <div className="header-bottom-right">
-            <span>2019-05-16 10:12:36</span>
-            <img src="" alt="" />
-            <span>晴</span>
+            <span>{currentTime}</span>
+        
+            <span style={{marginLeft:'20px'}}>{weather}</span>
           </div>
         </div>
       </div>
