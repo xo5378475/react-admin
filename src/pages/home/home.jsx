7e5a05d7fe1ed344d810289edfc76687477763b5
xo5378475 <xo5378475@gmail.com>
2022-04-06 00:46:21 +0800

    Home组件 简单界面

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
+import './home.less'
+
 
 export default class Home extends Component {
   render() {
     return (
-      <div>Home</div>
+      <div className='home'>
+        <h1>歡迎使用硅谷後台管理系統</h1>
+      </div>
     )
   }
 }
